6c72243adbd5705cf7627c56be49e82441d647ff
Mario beraha <berahamario@gmail.com>
2024-11-11 11:00:53 +0100

    performance improvements for pyp

--- julia_src/params.jl
+++ julia_src/params.jl
@@ -7,3 +7,27 @@ struct NGGParams
     alpha::Float64
     tau::Float64
 end
+
+mutable struct LogGenFactorialCoeffs
+    cache::Matrix{Float16}
+    sigma::Float64
+end
+
+
+function LogGenFactorialCoeffs(sigma::Float64)
+    cache = zeros(Float16, (1, 1))
+    cache[1, 1] = log(sigma)
+    return LogGenFactorialCoeffs(cache, sigma)
+end
+
+
+mutable struct PYParams
+    theta::Float64
+    alpha::Float64
+    fac_coeffs::LogGenFactorialCoeffs
+end
+
+
+function PYParams(theta::Float64, sigma::Float64)
+    return PYParams(theta, sigma, LogGenFactorialCoeffs(sigma))
+end
